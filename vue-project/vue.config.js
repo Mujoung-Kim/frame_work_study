0cb87de88889b6b85ec9792db46c2b0e778119d1
Arvens <lklkk2002@naver.com>
2023-03-13 15:36:28 +0900

    modify vue.config

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+// lazyLoad 구문 -> 적용시 위의 default 코드 삭제
+module.exports = {
+  chainWebpack : config => {
+    config.plugins.delete('prefetch')
+  }
+}
